cfba65f319887921c89417863111b3e34d7d877a
lihenga <1438677126@qq.com>
2026-08-29 09:24:28 +0800

    Add Mooncake aggregate prefetch token metric

--- mooncake-store/include/client_service.h
+++ mooncake-store/include/client_service.h
@@ -630,6 +630,12 @@ class Client {
         }
     }
 
+    void ObservePrefetchedTokens(uint64_t tokens) {
+        if (metrics_ != nullptr) {
+            metrics_->ObservePrefetchedTokens(tokens);
+        }
+    }
+
     void ObserveDirectSessionCache(bool hit) {
         if (metrics_ != nullptr) {
             metrics_->ObserveDirectSessionCache(hit);
